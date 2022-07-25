e03fed1a9fee0fdfaed39799b161fa0fb3ea259a
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-07-25 12:39:02 -0400

    api updated to specific alerts for specific states

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 
 weatherLink = document.getElementById("#weather");
-console.log ("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={3da63184704762e74a9d5e213f99d400}");
+//console.log ("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={3da63184704762e74a9d5e213f99d400}");
 
 function getApi() {
 
-    // Api Url
-    var urlRequest = "https://api.openweathermap.org/data/2.5/onecall?lat={35.61}&lon={83.55}&appid={3da63184704762e74a9d5e213f99d400}";
+     //ApiUrl
+    var urlRequest = "https://api.openweathermap.org/data/2.5/onecall?lat={35.61}&lon={83.55}&appid={d12707350df08c4703683ba822b2a53b}";
 
     fetch(urlRequest).then(function(response) {
         console.log(response);
@@ -24,7 +24,7 @@ getApi();
 function getApiTwo() {
 
     // Api Url
-    var urlRequestTwo = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=mBGn4CcseyYGFZxBpAU9GrVwE4YCxrhCtPngKKZ5";
+    var urlRequestTwo = "https://developer.nps.gov/api/v1/alerts?stateCode=NC&stateCode=TN&api_key=mBGn4CcseyYGFZxBpAU9GrVwE4YCxrhCtPngKKZ5";
 
     fetch(urlRequestTwo).then(function(response) {
         console.log(response);
